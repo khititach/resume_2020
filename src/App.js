0a36d2d9bf85962fe86e992e9b5c9600e8ad8363
khititach <khititach58@email.nu.ac.th>
2020-11-17 23:40:26 +0700

    edit import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { HashRouter as Router , Switch ,  Route  } from 'react-router-dom';
+import { BrowserRouter as Router , Switch ,  Route  } from 'react-router-dom';
 
 // import logo from './logo.svg';
 import './App.css';
