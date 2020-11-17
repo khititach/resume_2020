57e89dca32529fa944512d057f58ea62b134e56f
khititach <khititach58@email.nu.ac.th>
2020-11-17 23:26:56 +0700

    Updates

---
+++ precache-manifest.3a71e747e6c0ff66dd04d6601a73d636.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d84f7f4f3b8073b204e02c9db27429",
+    "url": "/resume_2020/index.html"
+  },
+  {
+    "revision": "3e60d46347059481c412",
+    "url": "/resume_2020/static/css/main.07be400b.chunk.css"
+  },
+  {
+    "revision": "dbc4982c8023d8339248",
+    "url": "/resume_2020/static/js/2.91c95af0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/resume_2020/static/js/2.91c95af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e60d46347059481c412",
+    "url": "/resume_2020/static/js/main.8e1a76a7.chunk.js"
+  },
+  {
+    "revision": "2e2b7f5e3fc9deb1ee9a",
+    "url": "/resume_2020/static/js/runtime-main.2bd74be9.js"
+  },
+  {
+    "revision": "0ad6b87ed8228bd4983e88c229faece8",
+    "url": "/resume_2020/static/media/bg.0ad6b87e.jpg"
+  },
+  {
+    "revision": "b9ddd9855c0240558874d356aa377ec4",
+    "url": "/resume_2020/static/media/gmail.b9ddd985.png"
+  },
+  {
+    "revision": "6676a70003ba4d61470e22c64d7ef379",
+    "url": "/resume_2020/static/media/html.6676a700.png"
+  },
+  {
+    "revision": "4a8c23476a7c20c5bee2a752a6f96e9e",
+    "url": "/resume_2020/static/media/instagram.4a8c2347.png"
+  },
+  {
+    "revision": "4677aa56ef40adf0775495a803b00983",
+    "url": "/resume_2020/static/media/javascript.4677aa56.png"
+  },
+  {
+    "revision": "b41f866df60d645ee4db0c1e30bac1e8",
+    "url": "/resume_2020/static/media/mongodb-com.b41f866d.png"
+  },
+  {
+    "revision": "60621585d2ba69986c44bdef2710ea60",
+    "url": "/resume_2020/static/media/mongodb.60621585.png"
+  },
+  {
+    "revision": "2d98b7eb65a148eaee141fe37efb294a",
+    "url": "/resume_2020/static/media/my.2d98b7eb.jpg"
+  },
+  {
+    "revision": "14c8050624574ddb4351c8af1aae60db",
+    "url": "/resume_2020/static/media/nodejs.14c80506.png"
+  },
+  {
+    "revision": "448ca321506e29744f5c46dc31bd05af",
+    "url": "/resume_2020/static/media/react.448ca321.png"
+  },
+  {
+    "revision": "8fc900ea01d027f922e11657b8d34fc8",
+    "url": "/resume_2020/static/media/sakura-logo.8fc900ea.png"
+  },
+  {
+    "revision": "ceffd8172eee165bff636c21ee2bdc3e",
+    "url": "/resume_2020/static/media/visual-studio-code.ceffd817.png"
+  }
+]);
